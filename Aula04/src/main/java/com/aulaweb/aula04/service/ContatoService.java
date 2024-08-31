0e36ad7f0f971f97257805004d6d3c2f3a6048f6
akiofabio <fabio.ikenoue@fatec.sp.gov.br>
2024-08-31 19:58:44 -0300

    Create ContatoService.java

---
+++ Aula04/src/main/java/com/aulaweb/aula04/service/ContatoService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aulaweb.aula04.service;
+
+/**
+ *
+ * @author Akio
+ */
+public class ContatoService {
+    
+}
