50d5f98f8e61ab4e8e2451e3ddcfa362c1363f61
akiofabio <fabio.ikenoue@fatec.sp.gov.br>
2024-08-31 21:26:54 -0300

    UP

--- Aula04/src/main/java/com/aulaweb/aula04/repository/ContatoRepository.java
+++ Aula04/src/main/java/com/aulaweb/aula04/repository/ContatoRepository.java
@@ -4,10 +4,150 @@
  */
 package com.aulaweb.aula04.repository;
 
+import com.aulaweb.aula04.model.ContatoModel;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author Akio
  */
 public class ContatoRepository {
+    private static String driver = "org.postgresql.Driver";
+    private static String url = "jdbc:mysql://localhost:3306/aula4?useSSL=false";
+    private static String user = "root";
+    private static String password = "123";
     
+    private static final String INSERT_CONTATOS_SQL = "INSERT INTO aula04.contatos" + "  (name, email) VALUES "
+			+ " (?, ?);";
+
+    private static final String SELECT_CONTATOS_BY_ID = "select id,name,email from aula04.contatos where id =?";
+    private static final String SELECT_ALL_CONTATOS = "select * from aula04.contatos";
+    private static final String DELETE_CONTATOS_SQL = "delete from aula04.contatos where id = ?;";
+    private static final String UPDATE_CONTATOS_SQL = "update aula04.contatos set name = ?,email= ?, country =? where id = ?;";
+
+    public ContatoRepository() {
+	}
+
+	protected Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+	public void insertUser(ContatoModel contato) throws SQLException {
+		System.out.println(INSERT_CONTATOS_SQL);
+		// try-with-resource statement will auto close the connection.
+		try (Connection connection = getConnection();
+				PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CONTATOS_SQL)) {
+			preparedStatement.setString(1, contato.getNome());
+			preparedStatement.setString(2, contato.getEmail());
+			System.out.println(preparedStatement);
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+	}
+
+	public ContatoModel selectUser(int id) {
+		ContatoModel contato = null;
+		// Step 1: Establishing a Connection
+		try (Connection connection = getConnection();
+				// Step 2:Create a statement using connection object
+				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_CONTATOS_BY_ID);) {
+			preparedStatement.setInt(1, id);
+			System.out.println(preparedStatement);
+			// Step 3: Execute the query or update query
+			ResultSet rs = preparedStatement.executeQuery();
+
+			// Step 4: Process the ResultSet object.
+			while (rs.next()) {
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				String country = rs.getString("country");
+				contato = new ContatoModel(id, name, email);
+			}
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+		return user;
+	}
+
+	public List<User> selectAllUsers() {
+
+		// using try-with-resources to avoid closing resources (boiler plate code)
+		List<User> users = new ArrayList<>();
+		// Step 1: Establishing a Connection
+		try (Connection connection = getConnection();
+
+				// Step 2:Create a statement using connection object
+			PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_CONTATOS);) {
+			System.out.println(preparedStatement);
+			// Step 3: Execute the query or update query
+			ResultSet rs = preparedStatement.executeQuery();
+
+			// Step 4: Process the ResultSet object.
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				String country = rs.getString("country");
+				users.add(new User(id, name, email, country));
+			}
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+		return users;
+	}
+
+	public boolean deleteUser(int id) throws SQLException {
+		boolean rowDeleted;
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(DELETE_CONTATOS_SQL);) {
+			statement.setInt(1, id);
+			rowDeleted = statement.executeUpdate() > 0;
+		}
+		return rowDeleted;
+	}
+
+	public boolean updateUser(User user) throws SQLException {
+		boolean rowUpdated;
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(UPDATE_CONTATOS_SQL);) {
+			statement.setString(1, user.getName());
+			statement.setString(2, user.getEmail());
+			statement.setString(3, user.getCountry());
+			statement.setInt(4, user.getId());
+
+			rowUpdated = statement.executeUpdate() > 0;
+		}
+		return rowUpdated;
+	}
+
+	private void printSQLException(SQLException ex) {
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+				System.err.println("Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null) {
+					System.out.println("Cause: " + t);
+					t = t.getCause();
+				}
+			}
+		}
+	}
 }
