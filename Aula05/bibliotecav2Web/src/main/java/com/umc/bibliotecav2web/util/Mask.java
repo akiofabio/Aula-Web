c5957223a22c9fbdcc5cbb02cc91ff76e3408057
akiofabio <fabio.ikenoue@fatec.sp.gov.br>
2024-10-05 17:07:50 -0300

    up

--- Aula05/bibliotecav2Web/src/main/java/com/umc/bibliotecav2web/util/Mask.java
+++ Aula05/bibliotecav2Web/src/main/java/com/umc/bibliotecav2web/util/Mask.java
@@ -11,17 +11,21 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
-
+//Classe para aplicar mascaras nos dados de saida
 public class Mask {
+    //Mascara que converte Date em String no formato dd/MM/uuuu
     public static String DataMask(Date data){
         LocalDate localDate = data.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        DateTimeFormatter formatters = DateTimeFormatter.ofPattern("d/MM/uuuu");
+        DateTimeFormatter formatters = DateTimeFormatter.ofPattern("dd/MM/uuuu");
         String texto = localDate.format(formatters);
         return texto;
     }
     
+    //Mascara que converte double em String com duas casas decimais
     public static String DinheiroMask(double valor){
+        //Escolhe o formato do decimal para duas casas apos a virgula
         DecimalFormat df = new DecimalFormat("0.00");
+        //Configura para o aredondamento para baixo
         df.setRoundingMode(RoundingMode.DOWN);
         String texto = df.format(valor);
         return texto;
